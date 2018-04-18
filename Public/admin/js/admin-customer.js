0f9f07805634bb1cf39302fe59945f3f86c8389f
yunjike30 <xiecongli@ycode.cn>
2018-04-18 10:02:52 +0800

    修改 Signed-off-by: yunjike30 <xiecongli@ycode.cn>

--- Public/admin/js/admin-customer.js
+++ Public/admin/js/admin-customer.js
@@ -2,24 +2,29 @@
 * @Author: xiecl
 * @Date:   2018-04-13 16:52:03
 * @Last Modified by:   congli.xie
-* @Last Modified time: 2018-04-13 18:00:28
+* @Last Modified time: 2018-04-16 17:14:58
 */
 new Vue({
 	el:'#answerNext',
 	data:{
 		list:[
-			{current:1,answer:'111'}
+			{current:1,answer:''}
 		],
+		mykey:1,
 	},
 	methods:{
 		addPush:function(){
 			let _self=this;
-			// 	_self.current+=1;
-			// let item={answer:''};
-			// _self.list.push(item);
+				_self.mykey=_self.mykey+1;
+			let item={current:_self.mykey,answer:''};
+			_self.list.push(item);
 		},
-		minusPush:function(){
-
+		minusPush:function(index){
+			let _self=this;
+				console.log(index);
+		},
+		gernerateId:function(prefix,index){
+			return prefix+index;
 		}
 	}
 })
