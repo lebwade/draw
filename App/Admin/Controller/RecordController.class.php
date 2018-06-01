20236d0bc213b1a1741d30d1a01b2c50d9ad7e86
congli <xiecongli@ipzoe.com>
2018-06-01 16:04:42 +0800

    奖项配置 Signed-off-by: congli <xiecongli@ipzoe.com>

--- App/Admin/Controller/RecordController.class.php
+++ App/Admin/Controller/RecordController.class.php
@@ -4,7 +4,7 @@
  * @Author: xiecl
  * @Date:   2018-05-31 19:09:14
  * @Last Modified by:   congli.xie
- * @Last Modified time: 2018-05-31 21:29:34
+ * @Last Modified time: 2018-06-01 15:58:30
  */
 namespace Admin\Controller;
 use Think\Controller;
@@ -83,6 +83,66 @@ class RecordController extends PublicController{
 		return $arr[$key];
 	}
 	public function lottery(){
+		$themeid =I('get.themeid');
+		$condition =array();
+		if($themeid){
+			$condition['themeid'] = $themeid;
+			$this->assign('themeid',$themeid);
+		}
+		//分页
+		$count   = $this->_prizeTable->where($condition)->count();// 查询满足要求的总记录数
+		$Page    = new \Think\Page($count,20);// 实例化分页类 传入总记录数和每页显示的记录数(25)
 
+		//头部描述信息，默认值 “共 %TOTAL_ROW% 条记录”
+		$Page->setConfig('header', '<li class="rows">共<b>%TOTAL_ROW%</b>条&nbsp;第<b>%NOW_PAGE%</b>页/共<b>%TOTAL_PAGE%</b>页</li>');
+		//上一页描述信息
+	    $Page->setConfig('prev', '上一页');
+	    //下一页描述信息
+	    $Page->setConfig('next', '下一页');
+	    //首页描述信息
+	    $Page->setConfig('first', '首页');
+	    //末页描述信息
+	    $Page->setConfig('last', '末页');
+	    $Page->setConfig('theme', '%FIRST%%UP_PAGE%%LINK_PAGE%%DOWN_PAGE%%END%%HEADER%');
+
+		$show  = $Page->show();// 分页显示输出
+
+		$list = $this->_prizeTable->where($condition)->limit($Page->firstRow.','.$Page->listRows)->order('shunxu desc,created desc')->select();
+		$new_list =array();
+		$userTable =M('user');
+		$themeTable =M('themes');
+		if(empty($list)){
+			foreach ($list as $key => $value) {
+				$info =$userTable->where(array('id'=>$value['uid']))->find();
+				$themeInfo =$themeTable->where(array('id'=>$value['theme_id']))->find();
+				$value['theme_title']=$themeInfo['theme_title'];
+				$value['nickname']=$themeInfo['name'];
+				$value['headimgurl']=$themeInfo['photo'];
+				$new_list []=$value;
+			}
+		}
+		$this->assign('themes',$themeTable->select());
+		$this->assign('list',$new_list);
+		$this->assign('page',$show);
+		$this->display(); // 输出模板
+	}
+	/**
+	 * 配置奖项设置
+	 * [config description]
+	 * @return [type] [description]
+	 */
+	public function config(){
+		$prizeGood =M('prize_good');
+		$prizeInfo = $prizeGood->order('id asc')->select();
+		if(IS_POST){
+			$post =I('post.');
+			foreach ($post['id'] as $key => $value) {
+				$prizeGood->where(array('id'=>$value))->save(array('probility'=>$post['probility'][$key],'limit_number'=>$post['limit_numberi'][$key]));
+			}
+
+			$this->ajaxReturn(array('error'=>0,'message'=>'保存成功'));
+		}
+		$this->assign('prizeInfo',$prizeInfo);
+		$this->display();
 	}
 }
